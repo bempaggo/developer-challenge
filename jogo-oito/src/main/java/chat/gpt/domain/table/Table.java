27ac8a1a8688e5acd652f16da7b18474e1cabc25
leomath42 <leonardomatheus042@gmail.com>
2022-06-07 12:10:20 -0300

    Fix random table verifying if is solvable

--- jogo-oito/src/main/java/chat/gpt/domain/table/Table.java
+++ jogo-oito/src/main/java/chat/gpt/domain/table/Table.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
 import chat.gpt.domain.listeners.NotificationListener;
 
-public class Table {
+public class Table implements Cloneable {
 
 	private NotificationListener listener;
 	private static Integer MAX_CELLS = 9;
@@ -19,25 +19,70 @@ public class Table {
 			cell.setValue(i);
 			cells.add(cell);
 		}
+
+		// cells.get(0).setValue(5);
+		// cells.get(1).setValue(2);
+		// cells.get(2).setValue(8);
+		// cells.get(3).setValue(4);
+		// cells.get(4).setValue(1);
+		// cells.get(5).setValue(7);
+		// cells.get(6).setValue(0);
+		// cells.get(7).setValue(3);
+		// cells.get(8).setValue(6);
+		this.randomizeTable();
+	}
+
+	public Table(Table table) {
+		this.listener = table.listener;
+
+		table.cells.stream().forEach(x -> {
+			var cell = new TableCell();
+			cell.setValue(x.getValue());
+			this.cells.add(cell);
+		});
+		// this.cells = new ArrayList<>(table.getCells());
 	}
 
 	public ArrayList<TableCell> getCells() {
 		return cells;
 	}
 
-	public void suffleTable() {
+	public void randomizeTable() {
 
 		Random rand = new Random();
 
-		for (Integer i = cells.size() - 1; i > 1; --i) {
-			Integer j = rand.nextInt(0, MAX_CELLS - 1);
-			var auxj = cells.get(j);
-			var auxi = cells.get(i);
+		do {
+			for (Integer i = cells.size() - 1; i > 1; --i) {
+				Integer j = rand.nextInt(0, MAX_CELLS - 1);
+				var auxj = cells.get(j);
+				var auxi = cells.get(i);
 
-			Integer aux = auxi.getValue();
-			auxi.setValue(auxj.getValue());
-			auxj.setValue(aux);
+				Integer aux = auxi.getValue();
+				auxi.setValue(auxj.getValue());
+				auxj.setValue(aux);
+			}
+		} while (!isSolvable());
+
+		System.out.println("OUT SUFFLE TABLE");
+	}
+
+	private Boolean isSolvable() {
+		// 5 2 8
+		// 4 1 7
+		// 0 3 6
+		Integer sum = 0;
+		for (Integer i = 0; i < cells.size(); ++i) {
+			Integer value = cells.get(i).getValue();
+
+			for (Integer j = i + 1; j < cells.size(); ++j) {
+				var auxValue = cells.get(j).getValue();
+				if (0 < auxValue && auxValue < value) {
+					sum += 1;
+				}
+			}
 		}
+
+		return sum % 2 == 0;
 	}
 
 	public void swap(Integer dx, Integer dy, Integer dw, Integer dz) {
@@ -66,7 +111,7 @@ public class Table {
 		return cells.get(cellIndex(dx, dy));
 	}
 
-	private Boolean jogoConcluido() {
+	public Boolean jogoConcluido() {
 		Boolean isGoal = false;
 
 		for (Integer i = 0; i < MAX_CELLS - 1; ++i) {
