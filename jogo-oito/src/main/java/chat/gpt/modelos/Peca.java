2e5ce05794d3cb7d4371fe9e0659dab5ade29f67
Felipe Lima <felipe.lima.092@ufrn.edu.br>
2023-05-25 18:42:10 -0300

    feat: criação da classe Peca

---
+++ jogo-oito/src/main/java/chat/gpt/modelos/Peca.java
@@ -0,0 +1,29 @@
+package chat.gpt.modelos;
+
+public class Peca {
+
+    int valor;
+
+    public Peca(int valor) {
+        this.valor = valor;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Peca peca = (Peca) o;
+
+        return valor == peca.valor;
+    }
+
+    @Override
+    public int hashCode() {
+        return valor;
+    }
+}
