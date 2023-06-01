052c723cf3693c655c8e89b76a64ee2e25dab3f4
Felipe Lima <felipe.lima.092@ufrn.edu.br>
2023-06-01 19:55:33 -0300

    feat: criação da classe botão para representar o botão na view

---
+++ jogo-oito/src/main/java/chat/gpt/src/view/Botao.java
@@ -0,0 +1,31 @@
+package chat.gpt.src.view;
+
+import javax.swing.*;
+
+public class Botao {
+
+    private JButton jButton;
+    private Integer valor;
+
+    public Botao(JButton jButton, Integer valor) {
+        this.jButton = jButton;
+        this.valor = valor;
+    }
+
+    public JButton getjButton() {
+        return jButton;
+    }
+
+    public void setjButton(JButton jButton) {
+        this.jButton = jButton;
+    }
+
+    public Integer getValor() {
+        return valor;
+    }
+
+    public void setValor(Integer valor) {
+        this.valor = valor;
+    }
+}
+
