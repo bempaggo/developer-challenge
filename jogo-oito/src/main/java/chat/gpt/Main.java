4e7caa298a7eeed5b7364f00f5a57b9bf848f1ec
leomath42 <leonardomatheus042@gmail.com>
2023-06-03 13:07:34 -0300

    - Partial commit, adding some game listener.

--- jogo-oito/src/main/java/chat/gpt/Main.java
+++ jogo-oito/src/main/java/chat/gpt/Main.java
@@ -3,21 +3,34 @@ package chat.gpt;
 import java.awt.event.KeyEvent;
 
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) {
         JButton[][] botoes = new JButton[3][3];
-
-        var table = new Table(botoes);
+        
         var listener = new Listener();
+        var gameListener = new GameListener();
+
+        var table = new Table(gameListener);
 
         listener
                 .subscribe(KeyEvent.VK_UP, new MoveAction(table, 1, 0))
                 .subscribe(KeyEvent.VK_DOWN, new MoveAction(table, -1, 0))
                 .subscribe(KeyEvent.VK_LEFT, new MoveAction(table, 0, 1))
-                .subscribe(KeyEvent.VK_RIGHT, new MoveAction(table, 0, -1));
+                .subscribe(KeyEvent.VK_RIGHT, new MoveAction(table, 0, -1))
+                .subscribe(KeyEvent.VK_ESCAPE, x -> {
+                    System.out.println("ESC " + " - Keycode: "+ x);
+                });
 
-        var game = new JogoDosOito(table, listener);
+
+        gameListener.subscribe("jogoConcluido", x -> {
+            // JOptionPane.showMessageDialog(this, "Parabéns, você venceu!");
+            JOptionPane.showMessageDialog(null, x);
+
+        });
+
+        var game = new JogoDosOito(table, listener, gameListener);
         game.start();
     }
 }
