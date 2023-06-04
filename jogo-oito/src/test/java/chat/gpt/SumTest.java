3a0a42c16faefbd69ab597c1e259c735cc90b2ea
leomath42 <leonardomatheus042@gmail.com>
2023-06-04 17:39:33 -0300

    Adding Junit5 and sample test

---
+++ jogo-oito/src/test/java/chat/gpt/SumTest.java
@@ -0,0 +1,12 @@
+package chat.gpt;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class SumTest {
+    @Test
+    void addition() {
+        assertEquals(2, 1+ 1);
+    }
+}
