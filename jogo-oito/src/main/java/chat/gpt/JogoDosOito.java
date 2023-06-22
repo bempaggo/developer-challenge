fcf035c037edd3acf1d1cb6a2d9a0e3600de3491
aleviannaf <alexandrevianna511@gmail.com>
2023-06-21 21:51:26 -0400

    refactor: desacoplei a logica do tabuleiro

--- jogo-oito/src/main/java/chat/gpt/JogoDosOito.java
+++ jogo-oito/src/main/java/chat/gpt/JogoDosOito.java
@@ -13,146 +13,95 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 public class JogoDosOito extends JFrame implements KeyListener {
+    private JButton[][] botoes = new JButton[3][3];
+    private JButton botaoReiniciar;
+    private Tabuleiro tabuleiroObjeto;
 
-	private int[][] tabuleiro = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 0 } };
-	private JButton[][] botoes = new JButton[3][3];
-	private JButton botaoReiniciar;
+    public JogoDosOito() {
+        super("Jogo dos Oito");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 300);
+        setLayout(new GridLayout(4, 3));
+        tabuleiroObjeto = new Tabuleiro( botoes);
 
-	public JogoDosOito() {
-		super("Jogo dos Oito");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(300, 300);
-		setLayout(new GridLayout(4, 3));
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                JButton botao = new JButton();
+                botao.setFont(new Font("Arial", Font.BOLD, 36));
+                botoes[i][j] = botao;
+                add(botao);
+            }
+        }
 
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				JButton botao = new JButton();
-				botao.setFont(new Font("Arial", Font.BOLD, 36));
-				botoes[i][j] = botao;
-				add(botao);
-			}
-		}
+        botaoReiniciar = new JButton("Reiniciar");
+        botaoReiniciar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                tabuleiroObjeto.reiniciarJogo();
+            }
+        });
+        add(new JLabel(""));
+        add(botaoReiniciar);
+        add(new JLabel(""));
 
-		botaoReiniciar = new JButton("Reiniciar");
-		botaoReiniciar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				reiniciarJogo();
-			}
-		});
-		add(new JLabel(""));
-		add(botaoReiniciar);
-		add(new JLabel(""));
+        addKeyListener(this);
+        setFocusable(true);
 
-		addKeyListener(this);
-		setFocusable(true);
-		atualizarTabuleiro();
-		setVisible(true);
-	}
+        tabuleiroObjeto.atualizarTabuleiro();
+        setVisible(true);
+    }
 
-	public void keyPressed(KeyEvent e) {
-		int keyCode = e.getKeyCode();
-		switch (keyCode) {
-		case KeyEvent.VK_UP:
-			mover(1, 0);
-			break;
-		case KeyEvent.VK_DOWN:
-			mover(-1, 0);
-			break;
-		case KeyEvent.VK_LEFT:
-			mover(0, 1);
-			break;
-		case KeyEvent.VK_RIGHT:
-			mover(0, -1);
-			break;
-		}
-	}
+    public void keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        switch (keyCode) {
+            case KeyEvent.VK_UP:
+                mover(1, 0);
+                break;
+            case KeyEvent.VK_DOWN:
+                mover(-1, 0);
+                break;
+            case KeyEvent.VK_LEFT:
+                mover(0, 1);
+                break;
+            case KeyEvent.VK_RIGHT:
+                mover(0, -1);
+                break;
+        }
+    }
 
-	public void keyTyped(KeyEvent e) {
-	}
+    public void keyTyped(KeyEvent e) {
+    }
 
-	public void keyReleased(KeyEvent e) {
-	}
+    public void keyReleased(KeyEvent e) {
+    }
 
-	private void mover(int linha, int coluna) {
-		int linhaVazia = -1;
-		int colunaVazia = -1;
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				if (tabuleiro[i][j] == 0) {
-					linhaVazia = i;
-					colunaVazia = j;
-				}
-			}
-		}
-		int novaLinha = linhaVazia + linha;
-		int novaColuna = colunaVazia + coluna;
-		if (novaLinha < 0 || novaLinha > 2 || novaColuna < 0 || novaColuna > 2) {
-			// movimento inválido
-			return;
-		}
-		tabuleiro[linhaVazia][colunaVazia] = tabuleiro[novaLinha][novaColuna];
-		tabuleiro[novaLinha][novaColuna] = 0;
-		atualizarTabuleiro();
-		if (jogoConcluido()) {
-			JOptionPane.showMessageDialog(this, "Parabéns, você venceu!");
-			reiniciarJogo();
-		}
-	}
+    private void mover(int linha, int coluna) {
+        tabuleiroObjeto.moverPeca(linha, coluna);
+        if (tabuleiroObjeto.jogoConcluido()) {
+            JOptionPane.showMessageDialog(this, "Parabéns, você venceu!");
+            tabuleiroObjeto.reiniciarJogo();
+        }
+    }
 
-	public static void main(String[] args) {
-		new JogoDosOito();
-	}
-
-	private boolean jogoConcluido() {
-		int count = 1;
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				if (tabuleiro[i][j] != count % 9) {
-					return false;
-				}
-				count++;
-			}
-		}
-		return true;
-	}
-
-	private boolean movimentarPeca(int linha, int coluna) {
-		if (linha > 0 && tabuleiro[linha - 1][coluna] == 0) {
-			tabuleiro[linha - 1][coluna] = tabuleiro[linha][coluna];
-			tabuleiro[linha][coluna] = 0;
-			return true;
-		} else if (linha < 2 && tabuleiro[linha + 1][coluna] == 0) {
-			tabuleiro[linha + 1][coluna] = tabuleiro[linha][coluna];
-			tabuleiro[linha][coluna] = 0;
-			return true;
-		} else if (coluna > 0 && tabuleiro[linha][coluna - 1] == 0) {
-			tabuleiro[linha][coluna - 1] = tabuleiro[linha][coluna];
-			tabuleiro[linha][coluna] = 0;
-			return true;
-		} else if (coluna < 2 && tabuleiro[linha][coluna + 1] == 0) {
-			tabuleiro[linha][coluna + 1] = tabuleiro[linha][coluna];
-			tabuleiro[linha][coluna] = 0;
-			return true;
-		}
-		return false;
-	}
-
-	private void atualizarTabuleiro() {
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				JButton botao = botoes[i][j];
-				int valor = tabuleiro[i][j];
-				if (valor == 0) {
-					botao.setText("");
-				} else {
-					botao.setText(String.valueOf(valor));
-				}
-			}
-		}
-	}
-
-	private void reiniciarJogo() {
-		tabuleiro = new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 0 } };
-		atualizarTabuleiro();
-	}
+   /* Pertencente ao código original (não mexido)
+    private boolean movimentarPeca(int linha, int coluna) {
+        if (linha > 0 && tabuleiro[linha - 1][coluna] == 0) {
+            tabuleiro[linha - 1][coluna] = tabuleiro[linha][coluna];
+            tabuleiro[linha][coluna] = 0;
+            return true;
+        } else if (linha < 2 && tabuleiro[linha + 1][coluna] == 0) {
+            tabuleiro[linha + 1][coluna] = tabuleiro[linha][coluna];
+            tabuleiro[linha][coluna] = 0;
+            return true;
+        } else if (coluna > 0 && tabuleiro[linha][coluna - 1] == 0) {
+            tabuleiro[linha][coluna - 1] = tabuleiro[linha][coluna];
+            tabuleiro[linha][coluna] = 0;
+            return true;
+        } else if (coluna < 2 && tabuleiro[linha][coluna + 1] == 0) {
+            tabuleiro[linha][coluna + 1] = tabuleiro[linha][coluna];
+            tabuleiro[linha][coluna] = 0;
+            return true;
+        }
+        return false;
+    }
+    */
 }
